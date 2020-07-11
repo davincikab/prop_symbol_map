54d1eba204c4da96ae9edf08eb76befda62b08e2
davincikab <davidnganganjeri079@gmail.com>
2020-07-11 18:02:13 +0300

    Color picker

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/ioservicedesk/cjyfq2zsf56wl1ds8p1hc6xb2', //stylesheet location
     center: [23.971756293963494, 30.03581303071013], // starting position
-    zoom: 1.45,// starting zoom
+    zoom: 1.67,// starting zoom
 });
+
+var colorPicker = document.getElementById('color-picker');
 
 map.on('load', function(e) {
     map.addSource('csvData', {
@@ -21,8 +23,9 @@ map.on('load', function(e) {
         source:'csvData',
         // filter:['has','point_count'],
         paint: {
-          'circle-color': '#f28cb1',
-          'circle-radius': ["get", "Count"]
+          'circle-color': "#00d8f5",
+          'circle-radius': ["get", "Count"],
+          'circle-opacity':0.8
         }
         
     });
@@ -31,7 +34,7 @@ map.on('load', function(e) {
     // add popup layer
     map.on('click', 'csvData', function(e) {
         // get the feature
-        var features = map.queryRenderedFeature(e.point, {layer:['csvData']});
+        var features = map.queryRenderedFeatures(e.point, {layers:['csvData']});
 
         if(!features[0]) {
             return;
@@ -40,14 +43,14 @@ map.on('load', function(e) {
         let feature = features[0];
 
         // create a popupContent
-        var description = "<div class='popup-container'><h3>" + feature.properties.NAME + "</h3>" + "<h4>" + "<b>" + "COMPANY: " + "</b> Hubs </h4>" + 
-                        "<h4>" + "<b>" + "SCORE: " + "</b>" + feature.properties.Count + "</h4> </div>";
+        var description = "<div class='popup-container'><h3>" + feature.properties.NAME + "</h3>" + 
+                        "<h4>" + "<b>" + "Hubs" + "</b>" + feature.properties.Count + "</h4> </div>";
         
         let coord = feature.geometry.coordinates;
-        new mapboxgl.PopUp()
-            .setLatLng(coord)
+        new mapboxgl.Popup()
+            .setLngLat(coord)
             .setHTML(description)
-            .addT(map);
+            .addTo(map);
     });
 
 
@@ -56,8 +59,13 @@ map.on('load', function(e) {
       });
 
       // Change it back to a pointer when it leaves.
-      map.on('mouseleave', 'csvData', function () {
+    map.on('mouseleave', 'csvData', function () {
         map.getCanvas().style.cursor = '';
-      });
+    });
+
+    // update the color
+    colorPicker.addEventListener('input', function(e) {
+        map.setPaintProperty('csvData', 'circle-color', e.target.value);
+    })
 
 });
